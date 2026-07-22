1d4bcc247ee45d9bc65cfcc2320e30141b3aeded
preranaxnaik <preranaxnaik@gmail.com>
2026-07-22 15:47:42 +0530

    made changes in ForLoopDemo java file

--- Day2_Loops/src/com/daytwo/loopingstatements/ForLoopDemo.java
+++ Day2_Loops/src/com/daytwo/loopingstatements/ForLoopDemo.java
@@ -5,6 +5,7 @@ public class ForLoopDemo {
 	public static void main(String[] args) {
 		for(int i = 1 ; i <= 5 ; i++ ) {
 			System.out.println("Welcome to For Loop Demo by TNS Learning");
+			System.out.println(i);
 		}
 
 	}
