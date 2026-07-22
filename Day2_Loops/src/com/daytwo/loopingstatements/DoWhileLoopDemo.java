74409c6889aba92345ff7fdc7e66731d9e5b914e
preranaxnaik <preranaxnaik@gmail.com>
2026-07-22 23:00:35 +0530

    implemented while, do-while and for-each loop in java

---
+++ Day2_Loops/src/com/daytwo/loopingstatements/DoWhileLoopDemo.java
@@ -0,0 +1,16 @@
+package com.daytwo.loopingstatements;
+
+public class DoWhileLoopDemo {
+
+	public static void main(String[] args) {
+		
+		int i = 1;
+		do {
+			System.out.println(i);
+			i++;
+		}
+		while(i <= 5 );
+
+	}
+
+}
---
+++ Day2_Loops/src/com/daytwo/loopingstatements/ForEachLoopDemo.java
@@ -0,0 +1,13 @@
+package com.daytwo.loopingstatements;
+
+public class ForEachLoopDemo {
+
+	public static void main(String[] args) {
+		int nums[] = {34 , 45 , 23 , 12 , 34};
+		for(int num : nums) {
+			System.out.println(num);
+		}
+
+	}
+
+}
