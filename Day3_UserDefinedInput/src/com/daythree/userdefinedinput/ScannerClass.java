afd7c8c3e771088eb82feb61cc81a01b1e449ff4
preranaxnaik <preranaxnaik@gmail.com>
2026-07-22 21:54:13 +0530

    added some special attributes

--- Day3_UserDefinedInput/src/com/daythree/userdefinedinput/ScannerClass.java
+++ Day3_UserDefinedInput/src/com/daythree/userdefinedinput/ScannerClass.java
@@ -9,27 +9,40 @@ public class ScannerClass {
 		int age = sc.nextInt();
 		sc.nextLine();
 		
+		//String Input
 		System.out.print("Enter the name : ");
 		String name = sc.nextLine();
 		
-		System.out.print("Enter the designation : ");
-		String desg = sc.next();
-		
-		System.out.print("Enter the salary : ");
-		float salary = sc.nextFloat();
-		
-		System.out.print("Enter the mobile number : ");
-		long mobile = sc.nextLong();
-		
-		
-		System.out.println("\nEmployee Details");
-        System.out.println("----------------");
-        System.out.println("Age         : " + age);
-        System.out.println("Name        : " + name);
-        System.out.println("Designation : " + desg);
-        System.out.println("Salary      : " + salary);
-        System.out.println("Mobile      : " + mobile);
-		
+        // Character input
+        System.out.print("Enter your gender (M/F): ");
+        char gender = sc.next().charAt(0);
+
+        // Float input
+        System.out.print("Enter your salary: ");
+        float salary = sc.nextFloat();
+
+        // Double input
+        System.out.print("Enter your percentage: ");
+        double percentage = sc.nextDouble();
+
+        // Long input
+        System.out.print("Enter your mobile number: ");
+        long mobile = sc.nextLong();
+
+        // Boolean input
+        System.out.print("Are you employed? (true/false): ");
+        boolean employed = sc.nextBoolean();
+
+        // Display the entered data
+        System.out.println("\n----- User Details -----");
+        System.out.println("Name       : " + name);
+        System.out.println("Age        : " + age);
+        System.out.println("Gender     : " + gender);
+        System.out.println("Salary     : " + salary);
+        System.out.println("Percentage : " + percentage);
+        System.out.println("Mobile     : " + mobile);
+        System.out.println("Employed   : " + employed);
+
 		
 		sc.close();
 	}
