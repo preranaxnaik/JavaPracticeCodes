abb08c4f0cd202df41a7950278d6752391b4fe19
preranaxnaik <preranaxnaik@gmail.com>
2026-08-06 17:47:25 +0530

    implemented method overloading in java

--- Day10_Polymorphism/src/com/day10/compiletimepolymorphism/Student.java
+++ Day10_Polymorphism/src/com/day10/compiletimepolymorphism/Student.java
@@ -4,19 +4,22 @@ package com.day10.compiletimepolymorphism;
 public class Student {
 	
 	public void studentDetails() {
-		System.out.println("Please enter your details ");
+		System.out.println("Please enter your details :");
 	}
 	
+	//number odf parameters
 	public void studentDetails(int id, String name) {
-		System.out.println("Please enter your details ");
+		System.out.println("Please enter your details :");
 	}
 	
+	//position of parameters
 	public void studentDetails(String name, int id) {
-		System.out.println("Please enter your details ");
+		System.out.println("Please enter your details :");
 	}
 	
+	//datatypes of parameters
 	public void studentDetails(String name, short id) {
-		System.out.println("Please enter your details ");
+		System.out.println("Please enter your details :");
 	}
 
 }
