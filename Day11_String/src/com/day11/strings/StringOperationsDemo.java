8cb51a378c0af063bb98c746fee3e7df414850aa
preranaxnaik <prerana91042@gmail.com>
2026-08-10 10:10:09 +0530

    implemented string operations

---
+++ Day11_String/src/com/day11/strings/StringOperationsDemo.java
@@ -0,0 +1,22 @@
+package com.day11.strings;
+
+public class StringOperationsDemo {
+	
+	public static void main(String[] args) {
+		//String s1 = "    Learning Java    ";
+		
+		String s1 = "We are Learning Java with SQL and JDBC ";
+		
+		System.out.println(s1.toUpperCase());
+		System.out.println(s1.toLowerCase());
+		System.out.println(s1.length());
+		String s2 = s1.trim();
+		System.out.println(s2.length());
+		System.out.println(s1.substring(7 , 29));
+		System.out.println(s1.substring(7));
+		System.out.println(s1.charAt(8));
+		System.out.println(s1.indexOf('a'));
+		System.out.println(s1.replace('a', 'm'));
+	}
+
+}
