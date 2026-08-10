706cbaa59bde3277d01eb3019ce907fc5d1c9194
preranaxnaik <prerana91042@gmail.com>
2026-08-10 10:11:08 +0530

    implemented string comparison

---
+++ Day11_String/src/com/day11/strings/StringComparison.java
@@ -0,0 +1,29 @@
+package com.day11.strings;
+
+public class StringComparison {
+
+	public static void main(String[] args) {
+		
+		String s1 = "Good Morning";
+		String s2 = "good morning";
+		String s3 = new String(s1);
+		
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+		
+		System.out.println(s1 + " Equals " + s3 + " -> " + s1.equals(s3));
+		System.out.println(s1 + " Equals " + s2 + " -> " + s1.equals(s2));
+		System.out.println(s1 + " Equals " + s2 + " -> " + s1.equalsIgnoreCase(s2));
+		System.out.println(s1 + " Equals " + s2 + " -> " + (s1 == s2));
+		
+		//compareTo
+		
+		System.out.println(s1.compareTo("GOOD MORNING"));
+		System.out.println(s1.compareTo(s3));
+		System.out.println(s1.compareTo(s2));
+		
+
+	}
+
+}
