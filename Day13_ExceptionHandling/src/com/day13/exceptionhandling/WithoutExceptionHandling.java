aa2e37f111562fd4cc5338b4bdc25f001ce01b45
preranaxnaik <prerana91042@gmail.com>
2026-08-14 09:34:45 +0530

    demonstrated class without exception handling

---
+++ Day13_ExceptionHandling/src/com/day13/exceptionhandling/WithoutExceptionHandling.java
@@ -0,0 +1,11 @@
+package com.day13.exceptionhandling;
+
+public class WithoutExceptionHandling {
+
+	public static void main(String[] args) {
+		System.out.println("The program shows Exception");
+		int data = 100 / 0;
+		System.out.println(data);
+	}
+
+}
