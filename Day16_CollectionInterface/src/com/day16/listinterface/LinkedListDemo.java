1d6d31ea78ac46eda461d4c804a7095d4f5d7b08
preranaxnaik <prerana91042@gmail.com>
2026-09-03 14:25:21 +0530

    implemented list interface using linkedlist

---
+++ Day16_CollectionInterface/src/com/day16/listinterface/LinkedListDemo.java
@@ -0,0 +1,23 @@
+package com.day16.listinterface;
+import java.util.LinkedList;
+import java.util.Vector;
+
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+		
+		LinkedList<String> names = new LinkedList<String>();
+		names.add("Priya");
+		names.add("Kajal");
+		names.addFirst("Vaishnavi");
+		names.addLast("Krushna");
+		
+		System.out.println(names);
+
+		
+		Vector<Integer> v = new Vector<Integer>();
+		
+	}
+
+}
